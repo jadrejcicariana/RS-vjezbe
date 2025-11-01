c66955693d988f28c2f5446113098ea1bdd64496
Ariana <ajadrejci@unipu.hr>
2025-11-01 19:27:45 +0100

    v9 update

--- RS1/v9.py
+++ RS1/v9.py
@@ -6,16 +6,3 @@ def ukloni_duplikate(lista):
     return list(set(lista))
 
 print(ukloni_duplikate(lista))
-
-
-#implementacija pomocnim skupom
-def ukloni_duplikate2(lista):
-    
-    lista2 = []
-    
-    for x in lista:
-        if x not in lista2:
-            lista2.append(x)
-    return lista2
-
-print(ukloni_duplikate2(lista))
