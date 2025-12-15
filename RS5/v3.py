9528e05ec4b6557ea4891b580db7cc2b69d81441
Ariana <ajadrejci@unipu.hr>
2025-12-15 14:04:40 +0100

    RS5-v3

---
+++ RS5/v3.py
@@ -0,0 +1,24 @@
+#get punoljetni
+
+
+from aiohttp import web
+
+korisnici = [
+{'ime': 'Ivo', 'godine': 25},
+{'ime': 'Ana', 'godine': 17},
+{'ime': 'Marko', 'godine': 19},
+{'ime': 'Maja', 'godine': 16},
+{'ime': 'Iva', 'godine': 22}
+]
+
+async def get_punoljetni(request):
+    punoljetni = [x for x in korisnici if x["godine"] > 17]
+    return web.json_response(punoljetni)
+
+app = web.Application()
+
+app.router.add_routes([
+    web.get('/punoljetni', get_punoljetni)
+])
+
+web.run_app(app, port=8082)
